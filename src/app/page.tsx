c12b98bd74f83deda2df825b35dc6f2d0e00f892
Anna Kuzmenko <84227066+AnnaKuzmenko17@users.noreply.github.com>
2024-09-30 23:34:27 +0300

    feature/authorization (#4)
    * feat: add signInUser fn, update auth config
    
    * feat: add react-toastify
    
    * feat: add content for signOut test
    
    fix: update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-const Home = () => {
+import { auth } from "@/auth";
+import { Button } from "@nextui-org/react";
+import { signOut } from "@/auth";
+
+const Home = async () => {
+  const session = await auth();
+
   return (
     <div>
-      <h1>start template</h1>
+      <h3 className="text-2xl semibold">user session data:</h3>
+      {session ? (
+        <div>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <form action={async () => {
+            'use server';
+
+            await signOut();
+          }}>
+            <Button type="submit" variant="bordered" color="primary">sign out</Button>
+          </form>
+        </div>
+      ) : (
+        <div>not signed in</div>
+      )}
     </div>
   );
 }
